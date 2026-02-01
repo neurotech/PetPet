b84804fc43926cacd33c13d6108a019d27762165
Tim Douglas <neurotech@gmail.com>
2026-02-02 10:48:11 +1100

    TOC and line endings update

--- PetPet.lua
+++ PetPet.lua
@@ -1,98 +1,98 @@
 local function FindAura(unit, spellID, filter)
-  for i = 1, 100 do
-    local aura = C_UnitAuras.GetAuraDataByIndex(unit, i, filter)
+    for i = 1, 100 do
+        local aura = C_UnitAuras.GetAuraDataByIndex(unit, i, filter)
 
-    if aura == nil then return nil end
+        if aura == nil then return nil end
 
-    if spellID == aura.spellId then
-      return aura
+        if spellID == aura.spellId then
+            return aura
+        end
     end
-  end
 end
 
 local function HasActivePet(numPets)
-  if C_PetJournal.IsCurrentlySummoned == nil then
-    return false
-  end
-
-  local activePet = false
-  local ownedPets = {}
-
-  for i = 1, numPets do
-    local petID, _, owned, _, _, _, _, _ = C_PetJournal.GetPetInfoByIndex(i)
-
-    if owned then
-      table.insert(ownedPets, petID)
+    if C_PetJournal.IsCurrentlySummoned == nil then
+        return false
     end
-  end
 
-  for i = 1, #ownedPets do
-    local petID = ownedPets[i]
-    local isSummoned = C_PetJournal.IsCurrentlySummoned(petID)
+    local activePet = false
+    local ownedPets = {}
 
-    if isSummoned then
-      activePet = true
+    for i = 1, numPets do
+        local petID, _, owned, _, _, _, _, _ = C_PetJournal.GetPetInfoByIndex(i)
+
+        if owned then
+            table.insert(ownedPets, petID)
+        end
     end
-  end
 
-  return activePet
+    for i = 1, #ownedPets do
+        local petID = ownedPets[i]
+        local isSummoned = C_PetJournal.IsCurrentlySummoned(petID)
+
+        if isSummoned then
+            activePet = true
+        end
+    end
+
+    return activePet
 end
 
 local function MountedFlyingOrVehicle()
-  local isRetail = WOW_PROJECT_ID == WOW_PROJECT_MAINLINE
-  local isMoPClassic = WOW_PROJECT_ID == WOW_PROJECT_MISTS_CLASSIC
-  local isFlyingOrVehicle = IsFlying("player") or UnitHasVehicleUI("player")
+    local isRetail = WOW_PROJECT_ID == WOW_PROJECT_MAINLINE
+    local isMoPClassic = WOW_PROJECT_ID == WOW_PROJECT_MISTS_CLASSIC
+    local isFlyingOrVehicle = IsFlying("player") or UnitHasVehicleUI("player")
 
-  if isMoPClassic or isRetail then
-    return isFlyingOrVehicle
-  end
+    if isMoPClassic or isRetail then
+        return isFlyingOrVehicle
+    end
 
-  return IsMounted() or isFlyingOrVehicle
+    return IsMounted() or isFlyingOrVehicle
 end
 
 local function InitialisePetPet()
-  local PetPetListener = CreateFrame("Frame")
-  PetPetListener:RegisterEvent("PLAYER_STARTED_MOVING")
+    local PetPetListener = CreateFrame("Frame")
+    PetPetListener:RegisterEvent("PLAYER_STARTED_MOVING")
 
-  PetPetListener:SetScript("OnEvent", function()
-    local favouritePets = {}
-    local numPets, numOwned = C_PetJournal.GetNumPets()
-    local petActive = HasActivePet(numPets) or C_PetJournal.GetSummonedPetGUID() ~= nil
+    PetPetListener:SetScript("OnEvent", function()
+        local favouritePets = {}
+        local numPets, numOwned = C_PetJournal.GetNumPets()
+        local petActive = HasActivePet(numPets) or C_PetJournal.GetSummonedPetGUID() ~= nil
 
-    -- Player is not:
-    -- In combat:
-    local canSummon = not UnitAffectingCombat("player")
-        -- Mounted / Flying / In a vehicle:
-        and not MountedFlyingOrVehicle()
-        -- Mind-controlling:
-        and not (UnitIsControlling("player") and UnitChannelInfo("player"))
-        -- Stealthed / Dead
-        and not IsStealthed() and not UnitIsGhost("player")
-        -- Camouflaged:
-        and not FindAura("player", 199483, "HELPFUL")
-        -- Has Invisibility:
-        and not FindAura("player", 32612, "HELPFUL")
-        -- Gas Greater Invisibility:
-        and not FindAura("player", 110960, "HELPFUL")
-        -- Does not have a pet summoned:
-        and not petActive
-        -- Has at least 1 pet in their Companions list:
-        and numOwned > 0
+        -- Player is not:
+        -- In combat:
+        local canSummon = not UnitAffectingCombat("player")
+            -- Mounted / Flying / In a vehicle:
+            and not MountedFlyingOrVehicle()
+            -- Mind-controlling:
+            and not (UnitIsControlling("player") and UnitChannelInfo("player"))
+            -- Stealthed / Dead
+            and not IsStealthed() and not UnitIsGhost("player")
+            -- Camouflaged:
+            and not FindAura("player", 199483, "HELPFUL")
+            -- Has Invisibility:
+            and not FindAura("player", 32612, "HELPFUL")
+            -- Gas Greater Invisibility:
+            and not FindAura("player", 110960, "HELPFUL")
+            -- Does not have a pet summoned:
+            and not petActive
+            -- Has at least 1 pet in their Companions list:
+            and numOwned > 0
 
-    if canSummon then
-      for i = 1, numPets do
-        local petID, _, owned, _, _, favorite, _, _ = C_PetJournal.GetPetInfoByIndex(i)
+        if canSummon then
+            for i = 1, numPets do
+                local petID, _, owned, _, _, favorite, _, _ = C_PetJournal.GetPetInfoByIndex(i)
 
-        if owned and favorite then
-          table.insert(favouritePets, tostring(petID))
+                if owned and favorite then
+                    table.insert(favouritePets, tostring(petID))
+                end
+            end
+
+            if #favouritePets > 0 then
+                C_PetJournal.SummonPetByGUID(favouritePets[math.random(#favouritePets)])
+            end
         end
-      end
-
-      if #favouritePets > 0 then
-        C_PetJournal.SummonPetByGUID(favouritePets[math.random(#favouritePets)])
-      end
-    end
-  end)
+    end)
 end
 
 local loadingEvents = CreateFrame("Frame")
@@ -100,20 +100,20 @@ loadingEvents:RegisterEvent("ADDON_LOADED")
 loadingEvents:RegisterEvent("PLAYER_ENTERING_WORLD")
 
 loadingEvents:SetScript(
-  "OnEvent",
-  function(_, event, arg1)
-    if event == "ADDON_LOADED" and arg1 == "PetPet" then
-      -- Clean up old config settings:
-      if PetPetDB ~= nil then
-        PetPetDB = {}
-      end
+    "OnEvent",
+    function(_, event, arg1)
+        if event == "ADDON_LOADED" and arg1 == "PetPet" then
+            -- Clean up old config settings:
+            if PetPetDB ~= nil then
+                PetPetDB = {}
+            end
 
-      loadingEvents:UnregisterEvent("ADDON_LOADED")
-    end
+            loadingEvents:UnregisterEvent("ADDON_LOADED")
+        end
 
-    if event == "PLAYER_ENTERING_WORLD" then
-      InitialisePetPet()
-      loadingEvents:UnregisterEvent("PLAYER_ENTERING_WORLD")
+        if event == "PLAYER_ENTERING_WORLD" then
+            InitialisePetPet()
+            loadingEvents:UnregisterEvent("PLAYER_ENTERING_WORLD")
+        end
     end
-  end
 )
--- dev.sh
+++ dev.sh
@@ -1,33 +1,37 @@
-echo "Building PetPet and installing to WoW directory."
+#!/bin/bash
+
+WOW_DIR="$HOME/games/World of Warcraft"
+ADDON_NAME="PetPet"
+
+TARGETS=(
+    "_retail_:Retail"
+    # "_classic_:Classic"
+    # "_classic_ptr_:Classic PTR"
+    # "_xptr_:Retail PTR (xptr)"
+    # "_ptr_:Retail PTR"
+)
+
+install_addon() {
+    local client="$1"
+    local label="$2"
+    local dest="$WOW_DIR/$client/Interface/AddOns/$ADDON_NAME"
+
+    echo "Copying assets to $label..."
+    mkdir -p "$dest"
+    cp *.lua "$dest/"
+    cp PetPet.toc.tmp "$dest/$ADDON_NAME.toc"
+}
+
+echo "Building $ADDON_NAME and installing to WoW directory."
 
 echo "Creating TOC file..."
-touch PetPet.toc.tmp
-cat PetPet.toc >PetPet.toc.tmp
-sed -i "s/@project-version@/$(git describe --abbrev=0)/g" PetPet.toc.tmp
+sed "s/@project-version@/$(git describe --abbrev=0)/g" PetPet.toc > PetPet.toc.tmp
 
-echo "Copying assets to Retail..."
-mkdir -p /c/Program\ Files\ \(x86\)/World\ of\ Warcraft/_retail_/Interface/AddOns/PetPet/
-cp *.lua /c/Program\ Files\ \(x86\)/World\ of\ Warcraft/_retail_/Interface/AddOns/PetPet/
-cp PetPet.toc.tmp /c/Program\ Files\ \(x86\)/World\ of\ Warcraft/_retail_/Interface/AddOns/PetPet/PetPet.toc
-
-echo "Copying assets to Classic..."
-mkdir -p /c/Program\ Files\ \(x86\)/World\ of\ Warcraft/_classic_/Interface/AddOns/PetPet/
-cp *.lua /c/Program\ Files\ \(x86\)/World\ of\ Warcraft/_classic_/Interface/AddOns/PetPet/
-cp PetPet.toc.tmp /c/Program\ Files\ \(x86\)/World\ of\ Warcraft/_classic_/Interface/AddOns/PetPet/PetPet.toc
-
-echo "Copying assets to Classic PTR..."
-mkdir -p /c/Program\ Files\ \(x86\)/World\ of\ Warcraft/_classic_ptr_/Interface/AddOns/PetPet/
-cp *.lua /c/Program\ Files\ \(x86\)/World\ of\ Warcraft/_classic_ptr_/Interface/AddOns/PetPet/
-cp PetPet.toc.tmp /c/Program\ Files\ \(x86\)/World\ of\ Warcraft/_classic_ptr_/Interface/AddOns/PetPet/PetPet.toc
-
-echo "Copying assets to Retail PTRs..."
-mkdir -p /c/Program\ Files\ \(x86\)/World\ of\ Warcraft/_xptr_/Interface/AddOns/PetPet/
-cp *.lua /c/Program\ Files\ \(x86\)/World\ of\ Warcraft/_xptr_/Interface/AddOns/PetPet/
-cp PetPet.toc.tmp /c/Program\ Files\ \(x86\)/World\ of\ Warcraft/_xptr_/Interface/AddOns/PetPet/PetPet.toc
-
-mkdir -p /c/Program\ Files\ \(x86\)/World\ of\ Warcraft/_ptr_/Interface/AddOns/PetPet/
-cp *.lua /c/Program\ Files\ \(x86\)/World\ of\ Warcraft/_ptr_/Interface/AddOns/PetPet/
-cp PetPet.toc.tmp /c/Program\ Files\ \(x86\)/World\ of\ Warcraft/_ptr_/Interface/AddOns/PetPet/PetPet.toc
+for target in "${TARGETS[@]}"; do
+    client="${target%%:*}"
+    label="${target##*:}"
+    install_addon "$client" "$label"
+done
 
 echo "Cleaning up..."
 rm PetPet.toc.tmp
